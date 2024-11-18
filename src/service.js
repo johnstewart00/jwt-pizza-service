8b0346e8b1afa7d11479582fa1a1cb815ff18974
johnstewart00 <stew.john560@gmail.com>
2024-11-18 10:30:53 -0700

    remove pizza-logger dependency

--- src/service.js
+++ src/service.js
@@ -5,7 +5,7 @@ const franchiseRouter = require("./routes/franchiseRouter.js");
 const version = require("./version.json");
 const config = require("./config.js");
 const metrics = require("./metrics.js");
-const Logger = require("pizza-logger");
+const Logger = require("./Logger.js");
 
 const app = express();
 app.use(express.json());
